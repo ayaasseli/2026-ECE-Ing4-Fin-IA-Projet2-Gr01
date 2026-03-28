27bc83826e616364984cdc0317e2a5851bfb0616
ayaasseli <aya.asseli@edu.ece.fr>
2026-03-28 20:20:33 +0100

    Update main.py

--- Aya-Asseli-Options-Strategies-Automatisees/src/main.py
+++ Aya-Asseli-Options-Strategies-Automatisees/src/main.py
@@ -1,2 +1,1 @@
-Le code a été développé et exécuté sur la plateforme QuantConnect, qui permet le backtesting et l’exécution de stratégies de trading algorithmique.
-L’algorithme a été adapté au framework Quantconnect (QCAlgorithm) afin d’utiliser les données de marché, les options et les outils de simulation intégrés.
+Ce projet est implémenté sur QuantConnect et utilise le framework QCAlgorithm pour le backtesting et l’exécution de la stratégie.
